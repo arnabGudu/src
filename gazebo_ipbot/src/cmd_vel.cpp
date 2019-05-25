a60913b16a151183b6527dbdb982b76fa5adbad9
arnabGudu <arnabgudu.gs@gmail.com>
2019-05-25 16:35:02 +0530

    farewell_AcerOne

--- gazebo_ipbot/src/cmd_vel.cpp
+++ gazebo_ipbot/src/cmd_vel.cpp
@@ -2,6 +2,7 @@
 #include "geometry_msgs/Twist.h"
 #include "std_msgs/Int32.h"
 #include "opencv2/opencv.hpp"
+#include "gazebo_ipbot/pid.h"
 
 using namespace cv;
 using namespace std;
@@ -11,7 +12,7 @@ class cmd_vel
 public:
 	cmd_vel(ros::NodeHandle _nh);
 	void callbackErr(const std_msgs::Int32::ConstPtr& _msg);
-	void callbackPid(const std_msgs::Int32::ConstPtr& _msg);
+	void callbackPid(const gazebo_ipbot::pid::ConstPtr& _msg);
 	
 private:
 	ros::NodeHandle nh;
@@ -22,14 +23,14 @@ private:
 	
 	float lastError;
 	int side, angle;
-	int kp, ki, kd;
+	int kp, ki, kd, vel, intg;
 };
 
 cmd_vel::cmd_vel(ros::NodeHandle _nh) : nh(_nh)
 {
 	angle = 0;  side = 0;
 	lastError = 0;
-	kp = 20;  ki = 260;  kd = 80;
+	kp = 20;  ki = 20;  kd = 80;  vel = 50;  intg = 0;
 
 	pub = nh.advertise<geometry_msgs::Twist>("qt_pi/cmd_vel", 1000);
 	subErr = nh.subscribe("error", 1000, &cmd_vel::callbackErr, this);
@@ -40,29 +41,23 @@ void cmd_vel::callbackErr(const std_msgs::Int32::ConstPtr& _msg)
 {	
 	int error = _msg->data;
 	int diff  = error - lastError;
-	float balance = (float)kp/1000 * error + (float)kd/1000 * diff;
+	intg += error;
+	float balance = (float)kp/1000 * error + (float)kd/1000 * diff + (float)ki/1000 * intg;
 	lastError = error;
 
 	cout<<"bal"<<balance<<endl;
 
 	msg.angular.z = balance;
-	msg.linear.x = (float)ki/1000;
+	msg.linear.x = (float)vel/1000;
 	pub.publish(msg);
 }
 
-void cmd_vel::callbackPid(const std_msgs::Int32::ConstPtr& _msg)
+void cmd_vel::callbackPid(const gazebo_ipbot::pid::ConstPtr& _msg)
 {	
-	long int data = _msg->data;
-
-	kp = data % 1000;
-	data /= 1000;
-
-	kd = data % 1000;
-	data /= 1000;
-
-	ki = data;
-
-	waitKey(10);		
+	kp = _msg->kp.data;
+	kd = _msg->kd.data;
+	ki = _msg->ki.data;
+	vel = _msg->vel.data;		
 }
 
 int main(int argc, char **argv)
--- gazebo_ipbot/src/img_cap.cpp
+++ gazebo_ipbot/src/img_cap.cpp
@@ -3,6 +3,7 @@
 #include "opencv2/opencv.hpp"
 #include "cv_bridge/cv_bridge.h"
 #include "std_msgs/Int32.h"
+#include "gazebo_ipbot/pid.h"
 
 using namespace cv;
 using namespace std;
@@ -25,7 +26,7 @@ private:
 	int h, s, v;
 	int H, S, V;
 	int ht, wd;
-	int kp, kd, ki;
+	int kp, kd, ki, vel;
 
 	void trackbar();
 	void pidTrackbar();
@@ -33,17 +34,18 @@ private:
 	void perform();	
 };
 
-
 //constructor
 image::image(ros::NodeHandle _nh) : it(_nh), nh(_nh)
 {
 	h = 0;	s = 0;	v = 0;
 	H = 255;  S = 255;  V = 80;
 	ht = 285; wd = 70;
-	kp = 20;  ki = 260;  kd = 80;
+	kp = 20;  ki = 20;  kd = 80, vel = 50;
+
+	//kp = 20; kd = 186; vel = 264;
 
 	pubErr = nh.advertise<std_msgs::Int32>("error", 1000);
-	pubPid = nh.advertise<std_msgs::Int32>("pid", 1000);
+	pubPid = nh.advertise<gazebo_ipbot::pid>("pid", 1000);
 	sub = it.subscribe("front_camera/image_raw", 1, &image::callback, this);
 	//trackbar();
 	pidTrackbar();
@@ -54,8 +56,48 @@ void image::callback(const sensor_msgs::ImageConstPtr& _msg)
 {
 	src = cv_bridge::toCvShare(_msg, "bgr8")->image;
 	waitKey(10);
-
 	perform();
+}
+
+void image::show()
+{
+	imshow("src", src);
+	//imshow("blr", blr);
+	//imshow("hsv", hsv);
+	//imshow("thres", thres);
+	//imshow("canny", canny);
+}
+ 
+void image::trackbar() 
+{
+	namedWindow("thres");
+	createTrackbar("h", "thres", &h, 255, NULL);
+	createTrackbar("s", "thres", &s, 255, NULL);
+	createTrackbar("v", "thres", &v, 255, NULL);
+	createTrackbar("H", "thres", &H, 255, NULL);
+	createTrackbar("S", "thres", &S, 255, NULL);
+	createTrackbar("V", "thres", &V, 255, NULL);
+}
+
+void image::pidTrackbar()
+{
+	namedWindow("src");
+	createTrackbar("ht", "src", &ht, 487, NULL);
+	createTrackbar("wd", "src", &wd, 487, NULL);
+
+	createTrackbar("vel", "src", &vel, 1000, NULL);
+	createTrackbar("kp", "src", &kp, 1000, NULL);
+	createTrackbar("kd", "src", &kd, 1000, NULL);
+	createTrackbar("ki", "src", &ki, 1000, NULL);	
+	
+}
+
+int main(int argc, char **argv)
+{
+	ros::init(argc, argv, "image");
+	ros::NodeHandle nh;
+	image img(nh);
+	ros::spin();
 }
 
 //perform
@@ -94,62 +136,41 @@ void image::perform()
 
 		cout<<"cX:"<<(int)min[i].center.x + 120;
 		cout<<" cY:"<<(int)min[i].center.y + ht;
-		cout<<" angle:"<<(int)min[i].angle<<endl;
+		//cout<<" angle:"<<(int)min[i].angle<<endl;
+		
+		if (min[i].size.width < min[i].size.height)
+			cout<<"angle1:"<<min[i].angle<<endl;
+		else
+			cout<<"angle2:"<<min[i].angle + 90<<endl;
 
 		avgX += (int)min[i].center.x;
 	}	
 
 	rectangle(src, r, Scalar(0,0,255), 2);
 	line(src, Point(323,0), Point(323, 487), Scalar(0, 255, 0), 2); 
+	circle(src, Point(avgX, 200), 4, Scalar(255,0,0), 2);
 	
 	if (contours.size() > 0)
 	{
-		std_msgs::Int32 msgErr, msgPid;
+		std_msgs::Int32 msgErr;
+		gazebo_ipbot::pid msgPid;
+
 		msgErr.data = avgX / contours.size() + 120 - 323;
-		msgPid.data = kp + kd * 1000 + ki * 1000000;
+
+		msgPid.kp.data = kp;
+		msgPid.kd.data = kd;
+		msgPid.ki.data = ki;
+		msgPid.vel.data = vel;
+
 		pubErr.publish(msgErr);
 		pubPid.publish(msgPid);
 	}
+	else
+	{
+		std_msgs::Int32 msgErr;
+		msgErr.data = 0;
+		pubErr.publish(msgErr);
+	}
+		
 	show();
-}
-
-void image::show()
-{
-	imshow("src", src);
-	//imshow("blr", blr);
-	//imshow("hsv", hsv);
-	//imshow("thres", thres);
-	//imshow("canny", canny);
-}
- 
-
-void image::trackbar()
-{
-	namedWindow("thres");
-	createTrackbar("h", "thres", &h, 255, NULL);
-	createTrackbar("s", "thres", &s, 255, NULL);
-	createTrackbar("v", "thres", &v, 255, NULL);
-	createTrackbar("H", "thres", &H, 255, NULL);
-	createTrackbar("S", "thres", &S, 255, NULL);
-	createTrackbar("V", "thres", &V, 255, NULL);
-}
-
-void image::pidTrackbar()
-{
-	namedWindow("src");
-	createTrackbar("ht", "src", &ht, 487, NULL);
-	createTrackbar("wd", "src", &wd, 487, NULL);
-
-	createTrackbar("kp", "src", &kp, 1000, NULL);
-	createTrackbar("kd", "src", &kd, 1000, NULL);
-	createTrackbar("ki", "src", &ki, 1000, NULL);
-}
-
-
-int main(int argc, char **argv)
-{
-	ros::init(argc, argv, "image");
-	ros::NodeHandle nh;
-	image img(nh);
-	ros::spin();
 }
